f2c1d4b27a008b6261d18000dfcfcad1b2e7239a
Mateusz Armatys <mateusz.armatys@gmail.com>
2013-03-06 22:26:30 +0100

    Parsing url templates

--- urltemplate.go
+++ urltemplate.go
@@ -1,25 +1,83 @@
 package neo2go
 
 import (
+	"fmt"
 	"regexp"
+	"strings"
 )
 
-var paramsRegExp *regexp.Regexp
+var paramsRe, listParamRe *regexp.Regexp
 
-func init() {
-	paramsRegExp = regexp.MustCompile("{[^}]+}")
+type urlParameter struct {
+	// Only used for list parameters.
+	Delimiter string
+	// Name of the parameter.
+	Name string
+	// If true, it means the parameter is to be used as URL query, after the '?' sign, as name=value pair.
+	Queried bool
 }
 
 type UrlTemplate struct {
-	template string
+	parameters []urlParameter
+	template   string
+}
+
+func (u *UrlTemplate) parse() error {
+	indices := u.paramIndices()
+	for _, indexPair := range indices {
+		paramString := u.template[indexPair[0]:indexPair[1]]
+		s := strings.Trim(paramString, "{}")
+
+		var err error = nil
+		if strings.HasPrefix(s, "-list|") {
+			err = u.parseList(s)
+		} else {
+			err = u.parseNamedParams(s)
+		}
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (u *UrlTemplate) paramIndices() [][]int {
-	return paramsRegExp.FindAllStringIndex(u.template, -1)
+	return paramsRe.FindAllStringIndex(u.template, -1)
 }
 
-func (u *UrlTemplate) parse() {
-	_ = u.paramIndices()
+func (u *UrlTemplate) parseList(s string) error {
+	matches := listParamRe.FindStringSubmatch(s)
+	if matches == nil || len(matches) != 3 {
+		return fmt.Errorf("Could not find match for the url parameter: %s", s)
+	}
+
+	var param urlParameter
+	param.Delimiter = matches[1]
+	param.Name = matches[2]
+	u.parameters = append(u.parameters, param)
+
+	return nil
+}
+
+func (u *UrlTemplate) parseNamedParams(s string) error {
+	queried := false
+
+	if s[0] == '?' {
+		queried = true
+		s = s[1:]
+	}
+
+	paramNames := strings.Split(s, ",")
+	for _, name := range paramNames {
+		var param urlParameter
+		param.Queried = queried
+		param.Name = name
+		u.parameters = append(u.parameters, param)
+	}
+
+	return nil
 }
 
 func (u *UrlTemplate) Render(params ...string) string {
@@ -32,5 +90,10 @@ func (u *UrlTemplate) String() string {
 
 func (u *UrlTemplate) UnmarshalJSON(data []byte) error {
 	u.template = string(data)
-	return nil
+	return u.parse()
+}
+
+func init() {
+	paramsRe = regexp.MustCompile("{[^}]+}")
+	listParamRe = regexp.MustCompile(`^-list\|([^\|]+)\|([^\s]+)$`)
 }
--- urltemplate_test.go
+++ urltemplate_test.go
@@ -24,3 +24,67 @@ func TestParsingIndices(t *testing.T) {
 		}
 	}
 }
+
+func TestParsingList(t *testing.T) {
+	tpl := new(UrlTemplate)
+	tpl.template = "http://localhost:7474/db/data/node/9/relationships/all/{-list|&|types}"
+	err := tpl.parse()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(tpl.parameters) != 1 {
+		t.Fatalf("Expected 1 parameter, but got %d", len(tpl.parameters))
+	}
+
+	param := tpl.parameters[0]
+
+	if param.Delimiter != "&" {
+		t.Fatalf("Expected '&' delimiter, but got %v", param.Delimiter)
+	}
+
+	if param.Queried {
+		t.Fatalf("Expected the 'Queried' property to be false.")
+	}
+
+	name := "types"
+	if param.Name != name {
+		t.Fatalf("Expected the parameter name to be %v, but got %v.", name, param.Name)
+	}
+}
+
+func TestParsingNamedParams(t *testing.T) {
+	tpl := new(UrlTemplate)
+	tpl.template = "http://localhost:7474/db/data/node/1/paged/traverse/{returnType}{?pageSize,leaseTime}"
+	err := tpl.parse()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := []urlParameter{
+		urlParameter{"", "returnType", false},
+		urlParameter{"", "pageSize", true},
+		urlParameter{"", "leaseTime", true},
+	}
+
+	if len(tpl.parameters) != len(expected) {
+		t.Fatalf("Expected 1 parameter, but got %d", len(tpl.parameters))
+	}
+
+	for i := 0; i < len(expected); i++ {
+		exemplary := expected[i]
+		parsed := tpl.parameters[i]
+
+		if exemplary.Delimiter != parsed.Delimiter {
+			t.Fatalf("Expected '%v' delimiter, but got %v", exemplary.Delimiter, parsed.Delimiter)
+		}
+
+		if exemplary.Queried != parsed.Queried {
+			t.Fatalf("Expected the 'Queried' property to be %v, but got %v.", exemplary.Queried, parsed.Queried)
+		}
+
+		if exemplary.Name != parsed.Name {
+			t.Fatalf("Expected the parameter name to be %v, but got %v.", exemplary.Name, parsed.Name)
+		}
+	}
+}
